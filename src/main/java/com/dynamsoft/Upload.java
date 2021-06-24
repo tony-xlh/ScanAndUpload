c1e6e60ecc1b2081919f63d04113671dad2fb9b0
xulihang <xulihanghai@163.com>
2021-06-24 16:40:51 +0800

    recreate the project with eclipse

--- src/main/java/com/dynamsoft/Upload.java
+++ src/main/java/com/dynamsoft/Upload.java
@@ -3,6 +3,7 @@ package com.dynamsoft;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.servlet.ServletException;
@@ -11,50 +12,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+/**
+ * Servlet implementation class Upload
+ */
 public class Upload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Upload() {
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
-		 * Constructor of the object.
-		 */
-	public Upload() {
-		super();
-	}
-
-	/**
-		 * Destruction of the servlet. <br>
-		 */
-	public void destroy() {
-		super.destroy(); // Just puts "destroy" string in log
-		// Put your code here
-	}
-
-	/**
-		 * The doGet method of the servlet. <br>
-		 *
-		 * This method is called when a form has its tag value method equals to get.
-		 * 
-		 * @param request the request send by the client to the server
-		 * @param response the response send by the server to the client
-		 * @throws ServletException if an error occurred
-		 * @throws IOException if an error occurred
-		 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.sendError(500, "Method not supported");
-	}
-
-	/**
-		 * The doPost method of the servlet. <br>
-		 *
-		 * This method is called when a form has its tag value method equals to post.
-		 * 
-		 * @param request the request send by the client to the server
-		 * @param response the response send by the server to the client
-		 * @throws ServletException if an error occurred
-		 * @throws IOException if an error occurred
-		 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String contentType = request.getContentType();
 	    if (contentType.contains("multipart/form-data")) {
 	        String filePath = getServletContext().getInitParameter("file-upload");
@@ -62,20 +36,16 @@ public class Upload extends HttpServlet {
 	        File output = new File(filePath + filePart.getSubmittedFileName());
 	        System.out.println(filePath + filePart.getSubmittedFileName());
 	        OutputStream outStream = new FileOutputStream(output);
-	        outStream.write(filePart.getInputStream().readAllBytes());
+	        
+	        InputStream in = filePart.getInputStream();
+	        byte[] buffer = new byte[in.available()];
+	        in.read(buffer, 0, in.available());
+	        filePart.getInputStream().read(buffer);
+	        outStream.write(buffer);
 	        outStream.close();
 	    } else {
 	    	response.sendError(500, "Only multipart/form-data supported");
 	    }
-	}
-
-	/**
-		 * Initialization of the servlet. <br>
-		 *
-		 * @throws ServletException if an error occurs
-		 */
-	public void init() throws ServletException {
-		// Put your code here
 	}
 
 }
